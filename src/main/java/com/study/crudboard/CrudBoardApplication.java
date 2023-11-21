e8f5ad531d9ca98895f7966ba40ea6ccf6f894e0
최재용 <choiyongyong0813@gmail.com>
2023-11-21 21:34:03 +0900

    1234~~

---
+++ src/main/java/com/study/crudboard/CrudBoardApplication.java
@@ -0,0 +1,14 @@
+package com.study.crudboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudBoardApplication.class, args);
+    }
+
+
+}
