f593ebc8ca9276dbf80bc3af2ce4a257e7000a20
최재용 <choiyongyong0813@gmail.com>
2023-12-04 21:41:43 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/study/crudboard/entity/Board.java
    #	src/main/resources/application.properties

---
+++ src/main/java/com/study/crudboard/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.study.crudboard.repository;
+
+import com.study.crudboard.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, User> {
+}
